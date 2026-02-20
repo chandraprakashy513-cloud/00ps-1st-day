b950ee4d4a310be64bc04b7132a917ea315203cf
chnadra parakash <chandraprakashy513@gmail.com>
2026-02-20 22:47:34 +0530

    how to create a variable

---
+++ variable.java
@@ -0,0 +1,17 @@
+//how to create a variable in java
+
+class variable
+{
+    public static void main(String[] args) {
+        int id = 101;
+        String name="chandra prakash yadav";
+        double height =5.8;
+        char gen = 'M';
+
+        System.out.println("Name="+name);
+        System.out.println("Id="+id);
+        System.out.println("Height="+height);
+        System.out.println("Gender="+gen);
+
+    }
+}
